7c163f85097d437fc3aad2a45696fa905b07c4e3
René Stalder <renestalder@protonmail.com>
2018-02-18 17:43:36 +0100

    fix: don't render vue files
    Ensure vue files are not rendered with handlebars while initalizing a project.

--- meta.js
+++ meta.js
@@ -21,5 +21,6 @@ module.exports = {
       'message': 'Author'
     },
   },
+  skipInterpolation: "**/*.vue",
   completeMessage: '{{#inPlace}}To get started:\n\n  npm install # Or yarn\n  npm run dev{{else}}To get started:\n\n  cd {{destDirName}}\n  npm install # Or yarn\n  npm run dev{{/inPlace}}'
 };
