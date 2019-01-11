6448e435b2e1bd15e3ae4fd60ec50f91cc3d10e5
René Stalder <renestalder@protonmail.com>
2019-01-11 14:59:20 +0100

    chore: fix baseURL setting

--- template/nuxtdown.config.js
+++ template/nuxtdown.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   api: function(isStatic) {
-    const baseURL = process.env.BASE_URL || "http://localhost:3000";
-    const browserBaseURL = !isStatic ? "" : baseURL;
+    const baseURL = "http://localhost:3000";
+    const browserBaseURL = !isStatic ? "" : process.env.BASE_URL;
 
     return {
       baseURL,
